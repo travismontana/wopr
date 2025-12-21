74ffab7e53b4c17841c894d466da68dd61e00c71
Bob Bomar <bob@bomar.us>
2025-12-20 19:28:27 -0600

    trying something new

--- systems/wopr-cam/apiscripts/app.py
+++ systems/wopr-cam/apiscripts/app.py
@@ -35,11 +35,53 @@ def capture():
     data = request.json
     game_id = data['game_id']
     subject = data['subject']
+    subject_name = data['subject_name']
     sequence = data['sequence']
 
     # Generate filepath from config
     filepath = imagefilename(game_id, subject)
-    
+
+    resolution = get_str('camera.default_resolution')
+    logger.info(f"Res: {resolution}")
+    width = get_int(f'camera.resolutions.{resolution}.width')
+    height = get_int(f'camera.resolutions.{resolution}.height')
+    logger.info(f"Capturing {width}x{height} to {filepath}")
+        
+    # Initialize camera (0 = default camera)
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
+    # Set resolution (may or may not work depending on camera)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    # Capture
+    ret, frame = cap.read()
+    if ret:
+        print(f"File: {filepath}")
+        cv2.imwrite(filepath, frame)
+
+    cap.release()
+    return filepath
+
+@app.route('/captureml', methods=['POST'])
+def capture_ml():
+    data = request.json
+    game_id = data['game_id']
+    subject = data['subject']
+    subject_name = data['subject_name']
+    sequence = data['sequence']
+
+    # Generate filepath from config
+    filepath1 = imagefilename(game_id, subject)
+    # Build full path
+    base_path = get_str('storage.base_path')
+    ml_subdir = "ml" # get_str('storage.ml_subdir')
+    ml_dir = Path(base_path) / ml_subdir / game_id
+    filename = filepath1.split('/')[-1]
+    filepath = ml_dir / f"{subject_name}-{filename}"
+    ensure_path(ml_dir)
+
     resolution = get_str('camera.default_resolution')
     logger.info(f"Res: {resolution}")
     width = get_int(f'camera.resolutions.{resolution}.width')
