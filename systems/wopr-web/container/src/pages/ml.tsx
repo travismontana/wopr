8b31850706c0f1295e5e6c2aa94e155bada01a2b
Bob Bomar <bob@bomar.us>
2025-12-22 21:11:51 -0600

    a

--- systems/wopr-web/container/src/pages/ml.tsx
+++ systems/wopr-web/container/src/pages/ml.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import "./css/theme.css";
 
-const apiUrl = (window as any).ENV?.WOPR_API_URL || "http://wopr-api:8000";
+const apiUrl = (window as any).ENV?.WOPR_API_URL || "https://wopr-api.studio.abode.tailandtraillabs.org";
 
 
 type StatusState =
