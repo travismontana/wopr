50c33aafc11151f376b8d5cb615fa254ebc41ac8
Bob Bomar <bob@bomar.us>
2025-12-22 19:19:50 -0600

    removing api proxy

--- systems/wopr-web/container/src/pages/ml.tsx
+++ systems/wopr-web/container/src/pages/ml.tsx
@@ -2,9 +2,15 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import "./css/theme.css";
 
 export default function MLPage() {
-    return (  <div className="p-4">
+    return (  
+    <div className="panel">
         <h1 className="text-2xl font-bold mb-4">Machine Learning Page</h1>
         <p>Welcome to the Machine Learning page of WOPR Web!</p>
+
+        <div className="actions">
+            <button className="button">Capture Image</button>
+            <button className="button">View Images</button>
+        </div>
     </div>
-    );
+    ); 
 }
