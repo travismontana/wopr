ed440c50c13357c91c61459fde4b6a6cb4d2d6f1
Bob Bomar <bob@bomar.us>
2025-12-24 12:56:59 -0600

    opentelemetry.sdk.resources~=1.39.1

--- pymods/wopr-core/wopr/tracing.py
+++ pymods/wopr-core/wopr/tracing.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
@@ -17,7 +18,7 @@ def create_tracer(tracer_name: str, tracer_enabled: bool, tracer_endpoint: str):
   })
   trace.set_tracer_provider(TracerProvider(resource=resource))
   tracer = trace.get_tracer(tracer_name)
-  
+
   span_processor = BatchSpanProcessor(otlp_exporter)
   trace.get_tracer_provider().add_span_processor(span_processor)
 
