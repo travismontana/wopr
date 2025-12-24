9d4af896a0eeb98aceca17dfafc7249e763ab6b7
Bob Bomar <bob@bomar.us>
2025-12-24 11:21:46 -0600

    Export-

--- pymods/wopr-core/wopr/tracing.py
+++ pymods/wopr-core/wopr/tracing.py
@@ -2,7 +2,7 @@
 
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
