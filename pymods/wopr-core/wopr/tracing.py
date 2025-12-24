c16083d6a50916728c2e234518a3ec75413be89d
Bob Bomar <bob@bomar.us>
2025-12-24 12:54:20 -0600

    tracemore

--- pymods/wopr-core/wopr/tracing.py
+++ pymods/wopr-core/wopr/tracing.py
@@ -11,12 +11,20 @@ def create_tracer(tracer_name: str, tracer_enabled: bool, tracer_endpoint: str):
   if not tracer_enabled:
       return None
     # Initialize OpenTelemetry
-  trace.set_tracer_provider(TracerProvider())
+  resource = Resource(attributes={
+      "service.name": tracer_name,
+      "service.version": tracer_version
+  })
+  trace.set_tracer_provider(TracerProvider(resource=resource))
   tracer = trace.get_tracer(tracer_name)
-
-  # Set up OTLP exporter
-  otlp_exporter = OTLPSpanExporter(endpoint=tracer_endpoint)
+  
   span_processor = BatchSpanProcessor(otlp_exporter)
   trace.get_tracer_provider().add_span_processor(span_processor)
 
   return tracer
+
+  tracing_enabled = woprconfig.get_bool(
+    "tracing.enabled", False,
+    service_name=woprvar.APP_NAME,
+    description=woprvar.APP_DESCRIPTION,
+    version=woprvar.APP_VERSION)
