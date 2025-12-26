3c633550a138b94def6f592c40083aff362110b8
Bob Bomar <bob@bomar.us>
2025-12-26 16:14:27 -0600

    a

--- pymods/wopr-core/wopr/config.py
+++ pymods/wopr-core/wopr/config.py
@@ -27,7 +27,7 @@ class ConfigClient:
         """
         if service_url is None:
             service_url = os.getenv(
-                'WOPR_API_URL', 'http://wopr-config_service.svc:8080')
+                'WOPR_API_URL', 'https://wopr-config_service.svc:8080')
             logging.debug(f"Using config service URL from environment: ({service_url})")
         else:
             logging.debug(f"Using provided config service URL: ({service_url})")
