840b8cc243661228b850038d209849161a0b5652
Bob Bomar <bob@bomar.us>
2025-12-26 23:26:28 -0600

    const apiUrl = (window as any).ENV?.WOPR_API_URL || "https://wopr-api.studio.abode.tailandtraillabs.org/ap1/v1";

--- systems/wopr-web/container/src/pages/status.tsx
+++ systems/wopr-web/container/src/pages/status.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import "./css/theme.css";
 
-const apiUrl = (window as any).ENV?.WOPR_API_URL || "https://wopr-api.studio.abode.tailandtraillabs.org";
+const apiUrl1 = (window as any).ENV?.WOPR_API_URL || "https://wopr-api.studio.abode.tailandtraillabs.org";
+
+const apiUrl = `${apiUrl1}/api/v1`;
 
 /*
 Sample status data:
