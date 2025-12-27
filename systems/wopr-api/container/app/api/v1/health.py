685a7c75b1db5a562085bdbd46957320fcfbf811
Bob Bomar <bob@bomar.us>
2025-12-27 10:52:28 -0600

    dt

--- systems/wopr-api/container/app/api/v1/health.py
+++ systems/wopr-api/container/app/api/v1/health.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from datetime import datetime, timezone
+import datetime
 from app import globals as woprvar
 logger = logging.getLogger(woprvar.APP_NAME)
 logging.basicConfig(filename="/var/log/wopr-api.log", level="DEBUG")
@@ -12,7 +13,7 @@ router = APIRouter(tags=["status"])
 @router.get("/", summary="Health Check", description="Check the health of the WOPR API.")
 @router.get("", summary="Health Check", description="Check the health of the WOPR API.")
 async def health_check() -> dict:
-    logger.debug("Performing health check at %s", datetime.datetime.datetime.now(datetime.timezone.utc).isoformat())
+    logger.debug("Performing health check at %s", datetime.now(datetime.timezone.utc).isoformat())
     return {
         "status": "ok",
         "service": "wopr-api",
