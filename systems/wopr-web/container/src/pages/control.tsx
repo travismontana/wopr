ca3f103cf65a171c9c7d1de4ec6745808075510a
Bob Bomar <bob@bomar.us>
2025-12-27 11:44:12 -0600

    ui

--- systems/wopr-web/container/src/pages/control.tsx
+++ systems/wopr-web/container/src/pages/control.tsx
@@ -1,6 +1,9 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./css/theme.css";
 
+const apiUrl1 = (window as any).ENV?.WOPR_API_URL || "https://wopr-api.studio.abode.tailandtraillabs.org";
+const apiUrl = `${apiUrl1}/api/v1`;
+
 
 export default function control() {
   return (
