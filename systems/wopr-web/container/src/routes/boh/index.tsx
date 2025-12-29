fb57b25daabfd453b99068bdefbbdbf60a0534e6
Bob Bomar <bob@bomar.us>
2025-12-29 11:19:07 -0600

    3

---
+++ systems/wopr-web/container/src/routes/boh/index.tsx
@@ -0,0 +1,22 @@
+import { Outlet, NavLink } from "react-router-dom"; 
+
+export default function BackOfHouse() { 
+  return ( 
+    <div className="boh"> 
+      <nav className="secondary-nav" aria-label="Back of House"> 
+        <ul>
+          <li><NavLink to="/boh/games/gamesmanager">Games Manager</NavLink></li>
+          <li><NavLink to="/boh/pieces/piecesmanager">Pieces Manager</NavLink></li>
+          <li><NavLink to="/boh/ml/mlimagesmanager">Machine Learning Images Manager</NavLink></li> 
+          <li><NavLink to="/boh/cameras">Cameras</NavLink></li> 
+          <li><NavLink to="/boh/images">Images</NavLink></li> 
+          <li><NavLink to="/boh/config">Configuration</NavLink></li> 
+          <li><NavLink to="/boh/status">Status</NavLink></li> 
+        </ul> 
+      </nav> 
+      <div className="boh-content"> 
+        <Outlet /> 
+      </div> 
+    </div> 
+  ); 
+} 
