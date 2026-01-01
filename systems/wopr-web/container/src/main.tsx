25e8b00bf890132c2213d3eb5f435322b6095a04
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-01 14:32:25 -0600

    theme

--- systems/wopr-web/container/src/main.tsx
+++ systems/wopr-web/container/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes } from "@generouted/react-router";
 
-import "./themes/modern.css";
+import "./themes/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
