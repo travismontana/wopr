7631ba47ebb470d5d8940f0f9ad88811db8f41ac
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-02 20:25:37 -0600

    DB styff

--- systems/wopr-api/container/app/globals.py
+++ systems/wopr-api/container/app/globals.py
@@ -16,8 +16,12 @@ APP_DOMAIN = "studio.abode.tailandtraillabs.org"
 APP_API_URL = "https://wopr-api." + APP_DOMAIN
 APP_OTEL_URL = "https://otel.monitoring.abode.tailandtraillabs.org"
 WOPR_API_URL = os.getenv('WOPR_API_URL', APP_API_URL+"/api/v1")
-DATABASE_URL = 'postgresql://' + os.getenv('WOPR_DB_USER') + ":" + os.getenv('WOPR_DB_PASSWORD') + "@wopr-config-db-cluster-rw.wopr.svc:5432/wopr-db"
-
+DATABASE_URL = 'postgresql://' + 
+    os.getenv('DBUSER') + ":" + 
+    os.getenv('DBPASSWORD') + "@" + 
+    os.getenv('DBHOST') + ":" + 
+    os.getenv('DBPORT') + "/" + 
+    os.getenv('DBNAME')
 APP_HOST = "0.0.0.0"
 APP_PORT = 8000
 
