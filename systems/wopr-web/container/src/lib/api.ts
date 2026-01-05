f60fe37ceaf4c229f417e33d2d49f223329725e8
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-04 22:16:07 -0600

    asd

--- systems/wopr-web/container/src/lib/api.ts
+++ systems/wopr-web/container/src/lib/api.ts
@@ -1,4 +1,4 @@
-const apiUrl = (window as any).ENV?.WOPR_API_URL || "https://api.wopr.tailandtraillabs.org";
+const apiUrl = "https://api.wopr.tailandtraillabs.org";
 const apiUrlFull = `${apiUrl}/api/v1`;
 
 export { apiUrl, apiUrlFull };
