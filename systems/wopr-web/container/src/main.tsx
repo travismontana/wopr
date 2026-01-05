5b8427c1fb5e615ba77e7666f52f99a2aff3c87b
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-04 19:33:04 -0600

    stuff

--- systems/wopr-web/container/src/main.tsx
+++ systems/wopr-web/container/src/main.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes } from "@generouted/react-router";
+
 import { ConfigContext } from "@/config/ConfigContext";
 import { fetchWoprConfig } from "@/config/fetchConfig";
 
-
 import "./themes/modern.css";
-
 
 async function bootstrap() {
   const config = await fetchWoprConfig();
+
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <Routes />
+      {/* ADDED: provide config to the whole app */}
+      <ConfigContext.Provider value={Object.freeze(config)}>
+        <Routes />
+      </ConfigContext.Provider>
     </React.StrictMode>
   );
 }
 
 bootstrap().catch((err) => {
   console.error("[WOPR] failed to bootstrap config", err);
-  document.body.innerHTML =
-    "WOPR BOOT FAILURE: could not load config.";
+  document.body.innerHTML = "WOPR BOOT FAILURE: could not load config.";
 });
