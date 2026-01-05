1c41fcab72f40220a039e62103054a55ecfe4855
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-04 18:54:21 -0600

    logger

--- systems/wopr-api/container/app/__init__.py
+++ systems/wopr-api/container/app/__init__.py
@@ -12,6 +12,7 @@ WOPR API application package.
 import logging
 from fastapi import APIRouter
 from app import globals as woprvar
+from app.logging import configure_logging
 
 router = APIRouter()
 logger = logging.getLogger(woprvar.APP_NAME)
--- systems/wopr-api/container/app/api/v2/__init__.py
+++ systems/wopr-api/container/app/api/v2/__init__.py
@@ -1,7 +1,8 @@
-# app/api/v1/__init__.py
+# app/api/v2/__init__.py
 import logging
 from fastapi import APIRouter
 from app import globals as woprvar
+from app.logging import configure_logging
 
 router = APIRouter()
 logger = logging.getLogger(woprvar.APP_NAME)
