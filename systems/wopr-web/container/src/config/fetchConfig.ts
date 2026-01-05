0a42505291bdd20172afbf7db944b371fc2d935f
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-04 19:22:40 -0600

    const res = await fetch(`${directusurl}/api/v2/config/all?environment=${environment}`, {

--- systems/wopr-web/container/src/config/fetchConfig.ts
+++ systems/wopr-web/container/src/config/fetchConfig.ts
@@ -1,10 +1,11 @@
 import type { WoprConfig } from "./appConfig";
 
-const directusurl = "https://directus.wopr.tailandtraillabs.org";
+//const directusurl = "https://directus.wopr.tailandtraillabs.org";
+const directusurl = "https://api.wopr.tailandtraillabs.org";
 const environment = "production";
 
 export async function fetchWoprConfig(): Promise<WoprConfig> {
-  const res = await fetch(`${directusurl}/items/woprconfig/1`, {
+  const res = await fetch(`${directusurl}/api/v2/config/all?environment=${environment}`, {
     headers: { Accept: "application/json" },
   });
 
