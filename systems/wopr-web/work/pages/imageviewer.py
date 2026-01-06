3ab63b19ce7111ddc4b57820af5e865c32bdcb2f
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-06 12:13:29 -0600

    web

--- systems/wopr-web/work/pages/imageviewer.py
+++ systems/wopr-web/work/pages/imageviewer.py
@@ -2,8 +2,6 @@ import streamlit as st
 import httpx
 import os 
 import requests
-#import streamlit_imagegrid
-from streamlit_image_gallery import streamlit_image_gallery
 
 st.title("WOPR ML Image View System")
 st.write("Welcome to the WOPR ML Image View System.")
