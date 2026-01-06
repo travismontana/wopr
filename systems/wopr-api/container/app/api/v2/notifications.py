85bb75681e1c6d52bbc99a0fb4e32f24af15ed78
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-05 18:12:20 -0600

    Adding notifications

---
+++ systems/wopr-api/container/app/api/v2/notifications.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+WOPR - Wargaming Oversight & Position Recognition
+# Copyright (c) 2025-present Bob Bomar <bob@bomar.us>
+# See git log for detailed authorship
+
+WOPR API - notifications
+"""
+
+"""
+  Endpoint:
+  POST /
+  Needs:
+  {
+  "username": "WOPR",
+  "embeds": [
+    {
+      "title": "Game Over",
+      "description": "Game ID 1234 has ended.",
+      "color": 16711680
+    }]}
+  
+"""
+from . import router, logger
+from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel, Field
+from app import globals as woprvar
+import requests
+
+router = APIRouter(tags=["notifications"])
+
+@router.post("/")
+async def create_notification(notification: dict):
+    """Create a notification"""
+    logger.info("Sending notification to Discord webhook")
+    try:
+        response = requests.post(woprvar.WOPRCONF['notifications']['discord']['webhook_url'], json=notification)
+        response.raise_for_status()
+        return {"detail": "Notification sent successfully"}
+    except requests.RequestException as e:
+        logger.error(f"Error sending notification: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error sending notification, error: {e}")
+
+
+
