cde0c280deb89329b448e73720f8b246dc827bdb
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-17 14:50:37 -0600

    f

--- systems/wopr-api/container/app/tasks/__init__.py
+++ systems/wopr-api/container/app/tasks/__init__.py
@@ -1,10 +1,10 @@
 # Copyright 2026 Bob Bomar
 # Licensed under the Apache License, Version 2.0
 
-from .image_tasks import *  # noqa
+#from .image_tasks import *  # noqa
 
 # Export tasks for discovery
-__all__ = [
-    'stage_images_to_labelstudio',
-    'archive_session',
-]
+#__all__ = [
+#    'stage_images_to_labelstudio',
+#    'archive_session',
+#]
