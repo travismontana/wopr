d9c1d8da9348847f01742f756757c33a3330a0f1
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-24 00:09:17 -0600

    d

--- systems/wopr-model/container/app/api/models.py
+++ systems/wopr-model/container/app/api/models.py
@@ -128,7 +128,8 @@ async def model_status(data: dict, request: Request):
         "filename": model_filename,
     }
     model_info.update(status_dict)
-    await update("models", model_id, model_info)
+    results = await update("models", model_id, model_info)
+    logit("Model info updated", model_info)
     return model_info
 
 
