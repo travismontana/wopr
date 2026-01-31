628272ee79ada135d492c1d1a9f1d1b7392187eb
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-31 13:56:51 -0600

    c

--- systems/wopr-model/container/app/lib/lib_model_ctl.py
+++ systems/wopr-model/container/app/lib/lib_model_ctl.py
@@ -18,23 +18,36 @@ def initialize_model(filename: str, model_family: str):
         filename (str): The filename to save the model to.
         model_family (str): The model family to initialize.
     """
-    logit("Filename: %s mod fam: %s" % (filename, model_family), "initialize_model")
-    if Path(f"{globals.WEIGHTS_PATH}/{filename}").exists():
-        logit(f"Model {filename} already exists", "initialize_model")
-        return {"status": "skipped", "message": "Model already exists."}
-    # let ultralytics handle the download if not found locally
     timenow = now().strftime("%Y%m%d%H%M%S")
+    fixed_filename = f"{globals.WEIGHTS_PATH}/{filename}"
+    fixed_backup_filename = f"{globals.WEIGHTS_PATH}/{timenow}_bak_{filename}"
+    logit("Filename: %s mod fam: %s" % (filename, model_family), "initialize_model")
+    if Path(f"/ultralytics/{fixed_filename}").exists():
+        mod = ultralytics.YOLO(fixed_filename)
+        logit(f"file {fixed_filename} exists, loading model", "initialize_model")
+        results = {
+            "status": "error",
+            "type": "model_exists",
+            "info": {
+                "mod_info": mod.info(),
+                "fixed_filename": fixed_filename,
+                "fixed_backup_filename": fixed_backup_filename,
+            },
+        }
+        return
+    # let ultralytics handle the download if not found locally
+
     mod_fam = ultralytics.YOLO(model_family)
-    mod_fam.save(f"{globals.WEIGHTS_PATH}/{timenow}_bak_{filename}")
-    mod_fam.save(f"{globals.WEIGHTS_PATH}/{filename}")
+    mod_fam.save(fixed_backup_filename)
+    mod_fam.save(fixed_filename)
     results = {
         "status": "success",
         "type": "model_info",
         "file_info": {
             "filename": filename,
-            "filepath": f"{globals.WEIGHTS_PATH}/{filename}",
+            "filepath": fixed_filename,
             "backup_filename": f"{timenow}_bak_{filename}",
-            "backup_filepath": f"{globals.WEIGHTS_PATH}/{timenow}_bak_{filename}",
+            "backup_filepath": fixed_backup_filename,
             "mod_info": mod_fam.info(),
         },
     }
