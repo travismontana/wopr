fbe9c4efdc80e31fe53437dc219d67a3958afe4c
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-01-31 14:02:53 -0600

    d

--- systems/wopr-model/container/app/lib/lib_model_ctl.py
+++ systems/wopr-model/container/app/lib/lib_model_ctl.py
@@ -22,6 +22,7 @@ def initialize_model(filename: str, model_family: str):
     fixed_filename = f"{globals.WEIGHTS_PATH}/{filename}"
     fixed_backup_filename = f"{globals.WEIGHTS_PATH}/{timenow}_bak_{filename}"
     logit("Filename: %s mod fam: %s" % (filename, model_family), "initialize_model")
+    logit(f"Checking if file {fixed_filename} exists", "initialize_model")
     if Path(f"/ultralytics/{fixed_filename}").exists():
         mod = ultralytics.YOLO(fixed_filename)
         logit(f"file {fixed_filename} exists, loading model", "initialize_model")
