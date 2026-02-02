8276e9a80a2fefe9d67c03b588d05c98ddd9e10b
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-01 23:40:47 -0600

    s

--- systems/wopr-model/container/app/lib/lib_model_ctl.py
+++ systems/wopr-model/container/app/lib/lib_model_ctl.py
@@ -217,7 +217,8 @@ def generate_dataset(dataset_uuid: str, dataset: dict):
                     )
 
                     # Extract filename and copy to images dir
-                    name = os.path.basename(local_image_path).split("__", 1)[-1]
+                    # name = os.path.basename(local_image_path).split("__", 1)[-1]
+                    name = os.path.basename(local_image_path)
                     destination_path = images_dir / name
                     shutil.copy2(local_image_path, destination_path)
 
