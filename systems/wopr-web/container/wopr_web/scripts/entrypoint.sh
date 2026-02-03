eb743d8ae69e38c0c2cbfca596743712002be9eb
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-03 14:48:13 -0600

    ave

---
+++ systems/wopr-web/container/wopr_web/scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# app/back_of_house/entrypoint.sh
+
+set -e
+
+echo "Waiting for postgres..."
+while ! nc -z ${DB_HOST:-localhost} ${DB_PORT:-5432}; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear  # ADD --clear to force refresh
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Starting gunicorn..."
+exec gunicorn --bind 0.0.0.0:8000 --workers 4 wopr_web.wsgi:application
