5116c2af5f4b1c6fa37250686e59cbc641f81b24
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-12 22:34:34 -0600

    s

--- systems/wopr-model/webui/app/pages/magic1.py
+++ systems/wopr-model/webui/app/pages/magic1.py
@@ -16,12 +16,10 @@ if uploaded_file is not None:
     img = cv.medianBlur(img, 5)
     cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     circles = cv.HoughCircles(
-        img, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0
+        img, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=50, maxRadius=0
     )
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
         cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-    st.image(
-        cimg, caption="Processed Image with Detected Circles", use_column_width=True
-    )
+    st.image(cimg, caption="Processed Image with Detected Circles")
