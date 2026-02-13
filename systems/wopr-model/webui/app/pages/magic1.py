51f7b2833828718b945898ec8dc9a7551851c388
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-13 00:15:01 -0600

    d

--- systems/wopr-model/webui/app/pages/magic1.py
+++ systems/wopr-model/webui/app/pages/magic1.py
@@ -33,7 +33,7 @@ if uploaded_file is not None:
     orig_img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
     img = cv.GaussianBlur(orig_img, (5, 5), 0)
     img_reg = cv.imdecode(file_bytes, cv.IMREAD_COLOR)
-    img = cv.medianBlur(img, 5)
+    img = cv.medianBlur(orig_img, 5)
     cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     circles = cv.HoughCircles(
         img, cv.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=100, maxRadius=0
