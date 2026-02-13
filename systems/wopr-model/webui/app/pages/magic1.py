944c9cfd4d823a4b60124c8f505233612bbfdcb9
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-12 22:28:08 -0600

    s

--- systems/wopr-model/webui/app/pages/magic1.py
+++ systems/wopr-model/webui/app/pages/magic1.py
@@ -1,3 +1,29 @@
 import streamlit as st
+import numpy as np
+import cv2 as cv
 
 st.title("Magic 1, ")
+
+# need a picture to work with.
+# let's ask the user to give us one.
+uploaded_file = st.file_uploader(
+    "Choose a picture to work with", type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+    img = cv.imread(uploaded_file.name, cv.IMREAD_GRAYSCALE)
+    assert (
+        img is not None
+    ), "Failed to read the uploaded image. Please ensure it's a valid image file."
+    img = cv.medianBlur(img, 5)
+    cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    circles = cv.HoughCircles(
+        img, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0
+    )
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+    st.image(
+        cimg, caption="Processed Image with Detected Circles", use_column_width=True
+    )
