2763e7ffdacda8b0788233786d39408efcc319a5
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-25 00:25:02 -0600

    mldataset_name = mldataset.uuid

--- systems/wopr-web/container/wopr_web/images/views.py
+++ systems/wopr-web/container/wopr_web/images/views.py
@@ -636,7 +636,7 @@ def create_mldatasets(request):
         game = Game.objects.filter(id=game_id).first()
         ls_project_id = request.POST.get("ls_project_id")
         mldataset, created = MLDataset.objects.update_or_create(game=game)
-        mldataset_name = mldataset.name
+        mldataset_name = mldataset.uuid
         logger.info(
             f"Creating ML dataset {mldataset_name} for game_id: {game_id}, ls_project_id: {ls_project_id}"
         )
