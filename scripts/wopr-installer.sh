f2bbffe4925603dd6b10b24a9419a2ff9344aae7
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-28 15:20:30 -0600

    save

---
+++ scripts/wopr-installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+pppid=$(ps -o comm= -p $(ps -o ppid= -p $$ | tr -d ' ' ))
+web_getters = ("curl", "wget", "fetch")
+for web_getter in "${web_getters[@]}"; do
+    if [[ ${pppid} == ${web_getter} ]]; then
+        echo "Don't curl | $0, it's bad juju"
+        exit 1
+    fi
+done
+
+declare -A commands_to_check_for
+commands_to_check_for["helm"]=helm
+commands_to_check_for["kubectl"]=kubectl
+
+for key in "${!commands_to_check_for[@]}"; do
+    installer=${commands_to_check_for[$key]}
+    if ! command -v $key &> /dev/null; then
+        echo "Command: ${key} is not installed, attempting to install..."
+        pamac install ${installer}
+    fi
+done
+
--- systems/wopr-web/container/wopr_web/cameras/views.py
+++ systems/wopr-web/container/wopr_web/cameras/views.py
@@ -4,6 +4,7 @@ import uuid
 import requests
 from pathlib import Path
 from lib.helpers import get_config, setup_logger
+from django.http import StreamingHttpResponse
 
 logger = setup_logger()
 config = get_config()
@@ -11,6 +12,31 @@ config = get_config()
 # Create your views here.
 def cameras_index(request):
     context = {}
+    c950_host = config["camera"]["camDict"]["2"]["host"]
+    c950_port = config["camera"]["camDict"]["2"]["port"]
+    c960_host = config["camera"]["camDict"]["1"]["host"]
+    c960_port = config["camera"]["camDict"]["1"]["port"]
+
+    c950_url = f"http://{c950_host}:{c950_port}/stream"
+    c960_url = f"http://{c960_host}:{c960_port}/stream"
+
+    c950_stream = requests.get(c950_url, stream=True)
+    c960_stream = requests.get(c960_url, stream=True)
+    streams = []
+    streams.append(
+        {
+            "c950_stream_resp": StreamingHttpResponse(
+                c950_stream.raw,
+                content_type="multipart/x-mixed-replace; boundary=frame",
+            ),
+            "c960_stream": StreamingHttpResponse(
+                c960_stream.raw,
+                content_type="multipart/x-mixed-replace; boundary=frame",
+            ),
+        }
+    )
+
+    context["streams"] = streams
     return render(request, "cameras_still_stream.html", context)
 
 
