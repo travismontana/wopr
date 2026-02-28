f0ce719a6d8698b43e972e12445553f4ee7460e0
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-27 20:40:49 -0600

    save

--- systems/wopr-model/webui/app/pages/live_circle2.py
+++ systems/wopr-model/webui/app/pages/live_circle2.py
@@ -135,10 +135,10 @@ def process_image(raw: bytes):
         resulting_image = circle_result
         return bgr, gray, resulting_image, notes
 
-    ratios = get_ratios(markers)
+    ratios = get_ratios(markers[0])
     notes.append({"ratios": ratios})
 
-    mark_circ_dist = get_distance(markers[0], circles[0], ratios)
+    mark_circ_dist = get_distance(markers[0], circles[0])
 
     if mark_circ_dist is not None:
         logger.info(f"Distance between marker and circle: {mark_circ_dist}px")
