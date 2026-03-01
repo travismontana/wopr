15a3799dccf31099a27f7672f60cf6dc8f9d6724
Bob Bomar <33264753+travismontana@users.noreply.github.com>
2026-02-28 18:32:42 -0600

    save

--- systems/wopr-web/container/wopr_web/cameras/views.py
+++ systems/wopr-web/container/wopr_web/cameras/views.py
@@ -20,10 +20,11 @@ def c950_stream(request):
     c950_host = config["camera"]["camDict"]["2"]["host"]
     c950_port = config["camera"]["camDict"]["2"]["port"]
     c950_url = f"http://{c950_host}:{c950_port}/stream"
+    logger.info(f"Accessing C950 stream at {c950_url}")
     c950_stream = requests.get(c950_url, stream=True)
     context["c950_stream"] = StreamingHttpResponse(
-        c950_stream.raw,
-        content_type="multipart/x-mixed-replace; boundary=frame",
+        c950_stream.iter_content(chunk_size=1024),
+        content_type="multipart/x-mixed-replace; boundary=boundarydonotcross",
     )
     return render(request, "cameras_still_stream.html", context)
 
@@ -33,10 +34,11 @@ def c960_stream(request):
     c960_host = config["camera"]["camDict"]["1"]["host"]
     c960_port = config["camera"]["camDict"]["1"]["port"]
     c960_url = f"http://{c960_host}:{c960_port}/stream"
+    logger.info(f"Accessing C960 stream at {c960_url}")
     c960_stream = requests.get(c960_url, stream=True)
     context["c960_stream"] = StreamingHttpResponse(
-        c960_stream.raw,
-        content_type="multipart/x-mixed-replace; boundary=frame",
+        c960_stream.iter_content(chunk_size=1024),
+        content_type="multipart/x-mixed-replace; boundary=boundarydonotcross",
     )
     return render(request, "cameras_still_stream.html", context)
 
